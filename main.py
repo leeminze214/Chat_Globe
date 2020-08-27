ad251d92971642f305fbe71a5a0262fc81c46151
leeminze214 <leeminz214@gmail.com>
2020-08-26 23:06:21 -0400

    auth file for secret key +gitignore

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from flask import Flask, redirect, render_template, request, url_for
 from flask_socketio import SocketIO
 from config import db_conn
 import psycopg2
+import json
+
+with open('auth.json') as auth:
+    secret = json.load(auth)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "(**#HRWOIFHN:LEHIKF(*#YHNL)#&)87293847290834htoirhgh9g8hs098hg"
+app.config['SECRET_KEY'] = secret['secret_key']
 io = SocketIO(app)
 params = db_conn()
 conn = psycopg2.connect(**params)
